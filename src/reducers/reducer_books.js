6af1523e0f7b5bb5dc76f7af42b3193bf1d6becd
eduarmreyes <eduar.mreyes@gmail.com>
2017-05-23 13:34:32 -0600

    Show book details on select with React

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{title: "Javascript: The Good Parts", pages: 101 },
+		{title: "Thr3s", pages: 39 },
+		{title: "The Dark Tower", pages: 97 },
+		{title: "Eloquent Ruby", pages: 23 },
+		{title: "Yet Another Programming Book", pages: 3 },
+	]
+}
